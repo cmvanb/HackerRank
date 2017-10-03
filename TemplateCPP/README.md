c966c0a93bb93344685211dc193d3dc9672c6dab
Casper <cmvanb@gmail.com>
2017-10-03 22:56:28 +0200

    add template code for easier hacking

---
+++ TemplateCPP/README.md
@@ -0,0 +1,12 @@
+# <insert_challenge_name_here>
+
+https://www.hackerrank.com/challenges/<insert_challenge_link_here>
+
+# Build
+
+* Make sure to include your own version of `libstdc++-6.dll`, I've included mine from GCC 6.3.0.
+* Configure the scons makefile (`SConstruct`) if necessary.
+* Run the following commands:
+
+`scons`  
+`main.exe`  
---
+++ TemplateCPP/main.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void insert_challenge_name_here()
+{
+    // EXAMPLE: input a single integer
+    // int input0;
+    // cin >> input0;
+
+    // EXAMPLE: input multiple integers on one line
+    // vector<int> inputs;
+    // int inputsCount = 10;
+
+    // string s;
+    // while (cin >> s)
+    // {
+    //     candleHeights.push_back(
+    //         atoi(s.c_str()));
+
+    //     if (inputs.size() >= inputsCount)
+    //     {
+    //         break;
+    //     }
+    // }
+}
+
+int main()
+{
+    insert_challenge_name_here();
+    return 0;
+}
