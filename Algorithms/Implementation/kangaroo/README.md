1d90500bbee2158d8a2713dc99c6b3a30965f484
Casper <cmvanb@gmail.com>
2017-10-03 23:27:35 +0200

    add kangaroo challenge code from template

---
+++ Algorithms/Implementation/kangaroo/README.md
@@ -0,0 +1,12 @@
+# kangaroo
+
+https://www.hackerrank.com/challenges/kangaroo
+
+# Build
+
+* Make sure to include your own version of `libstdc++-6.dll`, I've included mine from GCC 6.3.0.
+* Configure the scons makefile (`SConstruct`) if necessary.
+* Run the following commands:
+
+`scons`  
+`main.exe`  
---
+++ Algorithms/Implementation/kangaroo/main.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void kangaroo()
+{
+    // EXAMPLE: input a single integer
+    // int input0;
+    // cin >> input0;
+
+    // EXAMPLE: input multiple integers on one line
+    // vector<int> inputs;
+    // int inputsCount = 10;
+
+    // string s;
+    // while (cin >> s)
+    // {
+    //     candleHeights.push_back(
+    //         atoi(s.c_str()));
+
+    //     if (inputs.size() >= inputsCount)
+    //     {
+    //         break;
+    //     }
+    // }
+}
+
+int main()
+{
+    kangaroo();
+    return 0;
+}
